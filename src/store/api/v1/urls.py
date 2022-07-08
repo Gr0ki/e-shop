b120c26ad0252289847ac2930bf2e3803f647c45
Stanislav Finko <ghostzip8@gmail.com>
2022-07-08 19:56:31 +0300

    Update ProductList view. Create CRUD views for a product. Update urls.

--- src/store/api/v1/urls.py
+++ src/store/api/v1/urls.py
@@ -5,8 +5,10 @@ from .views import *
 
 urlpatterns = [
     # path('product_list', view_list_product, name='product-list'),
-    path('product_list', ProductList.as_view(), name='product-list'),
+    path('products', ProductList.as_view(), name='products-list'),
     # path('product/<int:product_id>', product, name='product'),
     # path('add_new_product', add_new_product, name='add-new-product'),
+    path('products/new', ProductCreate.as_view(), name='add-new-product'),
+    path('products/<int:id>', ProductRetrieveUpdateDestroy.as_view(), name='product-rud'),
 
 ]
