876ea21695febf9c7a1d40a6bb41faa9ca3785a2
Stanislav Finko <ghostzip8@gmail.com>
2022-08-24 19:06:25 +0300

    Update products serializers.

--- src/products/serializers.py
+++ src/products/serializers.py
@@ -1,8 +1,5 @@
 from rest_framework import serializers
 
-
-# from ..orders.models import OrderItem
-# from ..orders.serializers import OrderItemSerializer
 from .models import Category, Product
 
 
@@ -16,10 +13,6 @@ class CategorySerializer(serializers.ModelSerializer):
 
 
 class ProductSerializer(serializers.ModelSerializer):
-    # TODO: add authorization for stat fields
-    # ordered_quantity = serializers.SerializerMethodField()
-    # carts_items_quantity = serializers.SerializerMethodField()
-
     class Meta:
         model = Product
         fields = (
@@ -29,22 +22,4 @@ class ProductSerializer(serializers.ModelSerializer):
             "category",
             "description",
             "is_in_stock",
-            # "ordered_quantity",
-            # "carts_items_quantity",
         )
-
-    # def get_ordered_quantity(self, instance):
-    #     items = OrderItem.objects.filter(product=instance, order__isnull=False)
-    #     data = OrderItemSerializer(items, many=True).data
-    #     if data:
-    #         return sum((item["quantity"] for item in data))
-    #     else:
-    #         return 0
-
-    # def get_carts_items_quantity(self, instance):
-    #     items = OrderItem.objects.filter(product=instance, order__isnull=True)
-    #     data = OrderItemSerializer(items, many=True).data
-    #     if data:
-    #         return sum((item["quantity"] for item in data))
-    #     else:
-    #         return 0
